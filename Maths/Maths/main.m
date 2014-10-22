91592b96bc67d30ac22f46c738e754ff4d5437de
John Draper <jmdraper8@gmail.com>
2014-10-22 21:10:56 +1100

    First Class

---
+++ Maths/Maths/main.m
@@ -0,0 +1,60 @@
+//
+//  main.m
+//  Maths
+//
+//  Created by John Draper on 22/10/2014.
+//  Copyright (c) 2014 John Draper. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Fraction: NSObject
+
+@property int numerator, denominator;
+
+- (void) print;
+- (void) add: (Fraction *)f;
+- (void) reduce;
+
+
+@end
+
+@implementation Fraction
+
+@synthesize numerator, denominator;
+
+- (void) print {
+    
+    NSLog(@"%i/%i", numerator, denominator);
+}
+
+- (void) add:(Fraction *)f {
+ 
+    numerator = numerator * f.numerator + denominator * f.numerator;
+    denominator = denominator * f.denominator;
+}
+
+- (void)
+
+@end
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        Fraction *myFraction;
+        myFraction = [[Fraction alloc] init];
+        
+        
+        [myFraction setNumerator:10];
+        [myFraction setDenominator:22];
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+        [myFraction print];
+        
+        
+        
+    }
+    return 0;
+}
