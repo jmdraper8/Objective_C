9b65efee63b3f15dc5a03d17ee36e320caa121a6
John Draper <jmdraper8@gmail.com>
2014-10-28 21:31:45 +1100

    200 Assignments
    Starting

--- 200 Assignments/letter/letter/main.m
+++ 200 Assignments/letter/letter/main.m
@@ -11,7 +11,20 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        NSLog(@"+----------------------------------------------------------+\n");
+        NSLog(@"|                                                    ##### |\n");
+        NSLog(@"|                                                    ##### |\n");
+        NSLog(@"|                                                    ##### |\n");
+        NSLog(@"|                                                          |\n");
+        NSLog(@"|                                                          |\n");
+        NSLog(@"|                                  Steve Jobs              |\n");
+        NSLog(@"|                                  1 Infinite Loop         |\n");
+        NSLog(@"|                                  Cupertino               |\n");
+        NSLog(@"|                                                          |\n");
+        NSLog(@"+----------------------------------------------------------+\n");
+
+
+        
     }
     return 0;
 }
